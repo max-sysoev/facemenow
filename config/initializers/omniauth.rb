9bc5993d86f73e520857b9c6b0bdbe3b28a21ef8
mafiozzzza <mafiozzzza@ULapTop.(none)>
2012-07-02 11:58:13 +0400

    init

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '391712874210130', '469e236c9dba1d92ef1480169879907a'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facemenow::Application.config.secret_token = '09641bc1a252137cec2752ff1fec8b4a62c6356378b495dd60eeb8088bcccec1f5d72f9f2057b65bc3b4e4613887839f5a3c47b5da4ed60d6d72cfaf3a3d1b70'
